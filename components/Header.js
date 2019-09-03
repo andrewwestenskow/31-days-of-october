7df89b234e64953f8c84b40eff883e82698911e5
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-09-03 11:08:24 -0600

    Added header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import '../styles/Header.scss'
 
 const Header = (props) => {
   return (
     <div className='Header'>
-
+      <h2>We Watch Movies and then Talk About Them presents...</h2>
+      <h3>A curated festival of horror</h3>
+      <h1>31 Days of October</h1>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Header from '../components/Header'
 
 const Home = () => (
   <div className='Home'>
-
+    <Header/>
     <Footer/>
   </div>
 )
