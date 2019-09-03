84fc92d52c42770d578d147b4caa233f44a6a9d4
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-09-03 10:55:21 -0600

    Added Header

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../styles/Home.scss'
 import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 
 const Home = () => (
   <div className='Home'>
+
     <Footer/>
   </div>
 )
