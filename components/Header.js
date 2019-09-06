99e64a695f356af0addbf235f33e1fea5e89d576
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-09-06 12:17:07 -0600

    Mobile styling

--- components/Header.js
+++ components/Header.js
@@ -4,10 +4,9 @@ import '../styles/Header.scss'
 const Header = (props) => {
   return (
     <div className='Header'>
-      {/* <h2>We Watch Movies and then Talk About Them presents...</h2>
-      <h3>A curated festival of horror</h3> */}
       <h1>31 Days of October</h1>
-      <h2>Presented by We Watch Movies and then Talk About Them</h2>
+      <h3>A curated festival of horror</h3>
+      <h2>presented by We Watch Movies and then Talk About Them</h2>
     </div>
   )
 }
--- components/Week.js
+++ components/Week.js
@@ -4,8 +4,6 @@ import hulu from '../assets/hulu.png'
 import amazon from '../assets/amazon.png'
 import netflix from '../assets/netflix.png'
 import more from '../assets/more.png'
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const Week = (props) => {
@@ -23,27 +21,29 @@ const Week = (props) => {
               <p><strong>Runtime:</strong> {element.runtime} minutes</p>
               <p><strong>Released:</strong> {element.released}</p>
               <p className='synopsis'>{element.synopsis}</p>
-              <a className='streaming-link' target='blank' href={element.justWatch}>
-                <div className='streaming-hold'>
-                  <h3>Streaming options: </h3>
-                  <>{n ? <img className='streaming-logo' src={netflix} alt='netflix' /> : <></>}</>
-                  <>{h ? <img className='streaming-logo' src={hulu} alt='hulu' /> : <></>}</>
-                  <>{a ? <img className='streaming-logo' src={amazon} alt='amazon prime' /> : <></>}</>
-                  {/* <FontAwesomeIcon icon={faExternalLinkAlt} className='share-icon' /> */}
-                  <img className='streaming-logo' src={more}/>
-                </div>
-              </a>
+              <div className='streaming-hold'>
+                <h3>Streaming options: </h3>
+                <>{n ? <a href="https://www.netflix.com"><img className='streaming-logo' src={netflix} alt='netflix' /></a> : <></>}</>
+                <>{h ? <a href="https://www.hulu.com"><img className='streaming-logo' src={hulu} alt='hulu' /></a> : <></>}</>
+                <>{a ? <a target='blank' href="https://www.amazon.com/Prime-Video/b?ie=UTF8&node=2676882011"><img className='streaming-logo' src={amazon} alt='amazon prime' /></a> : <></>}</>
+                {/* <FontAwesomeIcon icon={faExternalLinkAlt} className='share-icon' /> */}
+                <a target='blank' href={element.justWatch}>
+                  <img className='streaming-logo' src={more} />
+                </a>
+              </div>
             </div>
             <img className='poster' src={element.poster} alt={element.title} />
             <p className='mobile-synopsis'>{element.synopsis}</p>
-            {/* {n || h || a ?
-                <div className='streaming-hold-mobile'>
-                  <h3>Stream it on:</h3>
-                  <>{n ? <img className='streaming-logo' src={netflix} alt='netflix' />: <></>}</>
-                  <>{h ? <img className='streaming-logo' src={hulu} alt='hulu' />: <></>}</>
-                  <>{a ? <img className='streaming-logo' src={amazon} alt='amazon prime' />: <></>}</>
-                </div> : <></>} */}
-            <a className='streaming-link-mobile' target='blank' href={element.justWatch}><FontAwesomeIcon icon={faExternalLinkAlt} className='share-icon' /></a>
+            <div className="streaming-hold-mobile">
+              <h3>Streaming options: </h3>
+              <>{n ? <a href="https://www.netflix.com"><img className='streaming-logo' src={netflix} alt='netflix' /></a> : <></>}</>
+              <>{h ? <a href="https://www.hulu.com"><img className='streaming-logo' src={hulu} alt='hulu' /></a> : <></>}</>
+              <>{a ? <a target='blank' href="https://www.amazon.com/Prime-Video/b?ie=UTF8&node=2676882011"><img className='streaming-logo' src={amazon} alt='amazon prime' /></a> : <></>}</>
+              {/* <FontAwesomeIcon icon={faExternalLinkAlt} className='share-icon' /> */}
+              <a target='blank' href={element.justWatch}>
+                <img className='streaming-logo' src={more} />
+              </a>
+            </div>
           </div>)
       })}
     </div>
