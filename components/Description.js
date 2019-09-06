4bcf652163c663dfa01cfc118e46dc5fdefdc113
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-09-06 15:41:42 -0600

    Added favicon

--- components/Description.js
+++ components/Description.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import '../styles/Description.scss'
-
+import bigger_movies from '../assets/bigger_movies.png'
 
 const Description = (props) => {
   return (
     <div className='Description'>
       <div className="description-box">
+          <img src={bigger_movies} alt="We watch movies" className="description-logo"/>
+          <article>
 
-      My name is andrew and I like movies
+          <h2>31 Days of October celebrates the best that horror has to offer.</h2>
+          <div className="line"></div>
+          <p className='description-text'>Each year we set aside the month of October to watch as many horror films as we can</p>
+          </article>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,21 @@ import Header from '../components/Header'
 import Description from '../components/Description'
 import Week from '../components/Week'
 import data from '../data/data'
+import Head from 'next/head'
 
 
 const Home = () => (
   <div className='Home'>
+    <Head>
+      <title>31 Days of October</title>
+      <link 
+      rel="shortcut icon" 
+      href="/static/bigger_movies.png" 
+      type="image/x-icon"/>
+    </Head>
     <Header />
-    {/* <Description /> */}
+    <Description />
+    {/* <div style={{maxWidth: '1200px', margin: '20px auto'}} className="line"></div> */}
     <div className="film-hold">
       {data.map(element => {
         return <Week key={element.week} data={element} />
